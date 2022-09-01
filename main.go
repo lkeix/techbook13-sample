5cf4a714b5ff6ac5fe644c2bd43a817c6d625cc5
lkeix <ai.kanaria.ai@gmail.com>
2022-09-02 02:21:54 +0900

    一旦簡単なパスパラメータはできるようになった

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/lkeix/techbookfes13-sample/router"
+)
+
+type value struct {
+	str     string
+	handler http.HandlerFunc
+}
+
+func main() {
+	values := []value{
+		{"/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("root"))
+		}},
+		{"/health", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("health"))
+		}},
+		{"/hey", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hey"))
+		}},
+		{"/:user/hey", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("health"))
+		}},
+		{"/hoge", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hoge"))
+		}},
+		{"/fuga", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("fuga"))
+		}},
+		{"/hoge/fuga", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hoge fuga"))
+		}},
+	}
+
+	r := router.NewRouter()
+
+	for _, v := range values {
+		r.Insert(v.str, v.handler)
+	}
+
+	http.ListenAndServe(":8080", r)
+}
