728fcbd4c6ee7453db2ac29e4569482891be2f47
lkeix <ai.kanaria.ai@gmail.com>
2022-12-09 23:29:39 +0900

    add: static, param, any path

--- tree/inspect_tree_test.go
+++ tree/inspect_tree_test.go
@@ -32,6 +32,36 @@ func TestInspectAdd(t *testing.T) {
 			path:   "/hoge/fuga",
 			h:      testHandler,
 		},
+		{
+			name:   "insert 1 nested path param path",
+			method: http.MethodGet,
+			path:   "/:user",
+			h:      testHandler,
+		},
+		{
+			name:   "insert 1 nested path param path, 1 nested static path",
+			method: http.MethodGet,
+			path:   "/:user/hoge",
+			h:      testHandler,
+		},
+		{
+			name:   "insert 1 nested static path, 1 nested param path",
+			method: http.MethodGet,
+			path:   "/hoge/:user",
+			h:      testHandler,
+		},
+		{
+			name:   "insert 1 nested any path",
+			method: http.MethodGet,
+			path:   "/hoge-*",
+			h:      testHandler,
+		},
+		{
+			name:   "insert 1 nested any path, 1 nested param path",
+			method: http.MethodGet,
+			path:   "/hoge-*/:user",
+			h:      testHandler,
+		},
 	}
 
 	n := NewNode()
