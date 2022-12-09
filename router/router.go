82467db0567ceeefb27306c7558d5967f80e2444
lkeix <ai.kanaria.ai@gmail.com>
2022-12-10 00:11:11 +0900

    add: inspect router

--- router/router.go
+++ router/router.go
@@ -19,10 +19,24 @@ type (
 		root *tree.Node
 		pool sync.Pool
 	}
+
+	routeMethod struct {
+		ppath   string
+		pnames  []string
+		handler http.HandlerFunc
+	}
+
+	kind      uint8
 	ParamsKey string
 )
 
-const paramskey ParamsKey = "params"
+const (
+	paramskey ParamsKey = "params"
+
+	staticKind kind = iota
+	paramKind
+	anyKind
+)
 
 func NewRouter() *Router {
 	return &Router{
@@ -41,6 +55,66 @@ func (o *Router) Insert(path string, handler http.HandlerFunc) {
 
 func (o *Router) Search(path string) {
 	o.root.Search(path)
+}
+
+func (o *Router) InspectAdd(method, path string, handler http.HandlerFunc) {
+	pnames := []string{}
+	ppath := path
+
+	if handler == nil {
+		panic("handler is nil")
+	}
+
+	for i, lcpIndex := 0, len(ppath); i < lcpIndex; i++ {
+		if path[i] == ':' {
+			if i > 0 && path[i-1] == '\\' {
+				path = path[:i-1] + path[i:]
+				i--
+				lcpIndex--
+				continue
+			}
+
+			j := i + 1
+			o.Inspectinsert(method, path[:i], staticKind, routeMethod{})
+			for ; i < lcpIndex && path[i] != '/'; i++ {
+			}
+
+			pnames = append(pnames, path[j:i])
+			path = path[:j] + path[i:]
+			i, lcpIndex = j, len(path)
+
+			if i == lcpIndex {
+				o.Inspectinsert(method, path[:i], paramKind, routeMethod{ppath, pnames, handler})
+			} else {
+				o.Inspectinsert(method, path[:i], paramKind, routeMethod{})
+			}
+		} else if path[i] == '*' {
+			o.Inspectinsert(method, path[:i], staticKind, routeMethod{})
+			pnames = append(pnames, "*")
+			o.Inspectinsert(method, path[:i+1], anyKind, routeMethod{ppath, pnames, handler})
+		}
+	}
+
+	o.Inspectinsert(method, path, staticKind, routeMethod{ppath, pnames, handler})
+}
+
+func (o *Router) Inspectinsert(method, path string, t kind, rm routeMethod) {
+	currentNode := o.root
+
+	if currentNode == nil {
+		panic("root is nil")
+	}
+
+	search := path
+	for {
+		searchLen := len(search)
+		prefixLen := len(currentNode.Prefix)
+
+		mx := prefixLen
+		if searchLen < mx {
+			mx = searchLen
+		}
+	}
 }
 
 func (o *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
